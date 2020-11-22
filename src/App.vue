6b81576e1b32d8692f282f9db7539b096e67708f
Johnny-KK <aabbusb@gmail.com>
2020-11-22 16:55:57 +0800

    添加Vant@3UI库

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
+
+  <van-button type="primary">主要按钮</van-button>
+<van-button type="success">成功按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
 </template>
 
 <style lang="scss">
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
 createApp(App)
   .use(store)
+  .use(Vant)
   .use(router)
   .mount("#app");
